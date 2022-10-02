a3a3e760caa368650f261d5125d7fc16fac4351e
Jaspr2020 <jsr5577@gmail.com>
2022-10-01 21:48:26 -0400

    Nothing works
    It doesn't work

--- popup.js
+++ popup.js
@@ -8,12 +8,26 @@ chrome.storage.sync.get("color", ({ color }) => {
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
+  
   chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: setPageBackgroundColor,
+    target: { tabId: tab.id},
+    func: sF,
   });
+
+  // chrome.scripting.executeScript({
+  //   target: { tabId: tab.id },
+  //   func: setPageBackgroundColor,
+  // });
 });
+
+function sF() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+  chrome.fontSettings.setFont( { fontId: 'asdf', genericFamily: 'standard'} )
+  // chrome.fontSettings.setFont( { fontId: 'Impact', genericFamily: 'standard'});
+  // chrome.fontSettings.setFont( { fontId: 'Impact', genericFamily: 'standard'});
+}
 
 // The body of this function will be executed as a content script inside the
 // current page
